56ffaad379110089fde389576e500dd6846841e2
Joshua Chuang <jcchuang@ucsd.edu>
2023-11-22 09:45:10 -0800

    add joshua.py

--- scripts/collect_images.py
+++ scripts/collect_images.py
@@ -8,10 +8,14 @@ SECRET = os.environ.get("SECRET")
 
 INPUT = 'data/Q1ProjectStructureList.xlsx'
 OUTPATH = 'data/images/'
+JOSHUA_INPUT = '../data/joshua_structures.json'
 
 
 from collect_images.phi import create_images as images_phi
+from collect_images.joshua import create_images as images_joshua
 
 
 if __name__== "__main__":
-    images_phi(INPUT, OUTPATH)
+    #images_phi(INPUT, OUTPATH)
+    images_joshua(JOSHUA_INPUT, OUTPATH)
+    
