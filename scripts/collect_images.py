77207eb84c635301336e070386fc32c695468edc
Phi <phi@sdge.com>
2023-12-04 20:01:13 -0800

    cleaner formating

--- scripts/collect_images.py
+++ scripts/collect_images.py
@@ -1,43 +1,21 @@
-
-#%%
-import os
-import requests
-from dotenv import load_dotenv
-load_dotenv()
-
-API_KEY = os.environ.get("API_KEY")
-SECRET = os.environ.get("SECRET")
+import images_individual as ii
 
 
 INPUT = 'data/Q1ProjectStructureList.xlsx'
 OUTPATH = 'data/images/'
-JOSHUA_INPUT = 'data/joshua_structures.json'
-
-
-from collect_images.phi import create_images as images_phi
-from collect_images.alex import get_dataset as images_alex
-from collect_images.brandon import collect_imgs as images_brandon
-from collect_images.kevin import create_images as images_kevin
-from collect_images.sunny import collect_struct_json as images_sunny
-from collect_images.jonathan import get_images as images_jonathan
-from collect_images.derek import derek_create_images
-from collect_images.kelly import collect_imgs as images_kelly
-from collect_images.noel import main as images_noel
-from collect_images.joshua import create_images as images_joshua
-from collect_images.tram import create_images as images_tram
-from collect_images.mateo import generate_images as images_mateo
 
 
 if __name__== "__main__":
-    images_phi(INPUT, OUTPATH)
-    images_alex()
-    images_brandon(OUTPATH)
-    images_kevin('data/kevin_structures.json', OUTPATH)
-    images_sunny('data/structure_coordinates.json', OUTPATH)
-    images_jonathan('data/jonathan_structures.json', OUTPATH)
-    derek_create_images()
-    images_kelly(OUTPATH)
-    images_noel(OUTPATH)
-    images_joshua(JOSHUA_INPUT, OUTPATH)
-    images_tram(OUTPATH)
-    images_mateo(OUTPATH)
+    ii.images_phi(INPUT, OUTPATH)
+    ii.images_alex()
+    ii.images_brandon(OUTPATH)
+    ii.images_kevin('data/kevin_structures.json', OUTPATH)
+    ii.images_sunny('data/structure_coordinates.json', OUTPATH)
+    ii.images_jonathan('data/jonathan_structures.json', OUTPATH)
+    ii.images_derek()
+    ii.images_kelly(OUTPATH)
+    ii.images_noel(OUTPATH)
+    ii.images_joshua('data/joshua_structures.json', OUTPATH)
+    ii.images_tram(OUTPATH)
+    ii.images_mateo(OUTPATH)
+    ii.images_lauren()
---
+++ scripts/images_individual/__init__.py
@@ -0,0 +1,13 @@
+from .phi import create_images as images_phi
+from .alex import get_dataset as images_alex
+from .brandon import collect_imgs as images_brandon
+from .kevin import create_images as images_kevin
+from .sunny import collect_struct_json as images_sunny
+from .jonathan import get_images as images_jonathan
+from .derek import derek_create_images as images_derek
+from .kelly import collect_imgs as images_kelly
+from .noel import main as images_noel
+from .joshua import create_images as images_joshua
+from .tram import create_images as images_tram
+from .mateo import generate_images as images_mateo
+from .lauren import create_images as images_lauren
